4cdefee019e0f65557073a53b2e72cd0cedf085f
Rajan Singh <rajan1992@hotmail.com>
2020-12-30 15:47:15 -0500

    Set the min height and viewport height

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -7,7 +7,7 @@
   export let segment: string;
 </script>
 
-<div class="bg-white dark:bg-black text-black dark:text-white h-full">
+<div class="bg-white dark:bg-black text-black dark:text-white h-screen">
   <div class="relative max-w-screen-xl mx-auto">
     <Nav {segment} />
     <main class="p-8">
