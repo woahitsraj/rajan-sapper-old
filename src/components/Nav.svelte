46dab4d8c2eb967fd8bd6e83b9388592a4fd57e2
Rajan Singh <rajan1992@hotmail.com>
2020-12-30 15:24:23 -0500

    Finish dark mode toggle and cleanup styling

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,21 +1,24 @@
+
 <script lang="ts">
   import DarkModeToggle from "./DarkModeToggle.svelte";
   export let segment: string;
+  const links = [
+    { text: "Home", route: "/" },
+    { text: "Blog", route: "/blog" },
+  ];
 </script>
 
-<nav class="flex flex-row py-8">
+<nav class="flex flex-row py-8 justify-between px-4">
+  <div />
   <ul class="flex flex-row">
-    <li>
-      <a
-        aria-current={segment === undefined ? 'page' : undefined}
-        href="/">Home</a>
-    </li>
-    <li>
-      <a
-        rel="prefetch"
-        aria-current={segment === 'blog' ? 'page' : undefined}
-        href="/blog">Blog</a>
-    </li>
+    {#each links as link}
+      <li>
+        <a
+          class="text-xl mx-4"
+          aria-current={segment === link.route.slice(1) ? 'page' : undefined}
+          href={link.route}>{link.text}</a>
+      </li>
+    {/each}
   </ul>
   <DarkModeToggle />
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,32 @@
+<script context="module">
+  export async function preload() {
+    const res = await this.fetch(`https://nomadlist.com/@woahitsraj.json`);
+    const nomadlistData = await res.json();
+
+    return { nomadlistData };
+  }
+</script>
+
+<script>
+  export let nomadlistData;
+  $: currentLocation = nomadlistData.location.now;
+</script>
+
 <svelte:head>
   <title>Rajan Singh</title>
 </svelte:head>
-
-<div class="flex w-full h-full">
+<div class="flex flex-col w-full h-full">
+  <span class="w-full">
+    <h1 class="text-6xl font-bold text-center mb-4">Woah! It's Raj!</h1>
+  </span>
   <span class="w-full">
     <img
-      class="w-32 h-32 rounded-full mr-4 inline float-left"
+      class="w-64 h-64 rounded-full mr-4 inline float-left"
       src="/me.jpg"
       alt="Me, Rajan Singh" />
-    <h1 class="text-6xl font-bold">Woah! It's Raj!</h1>
-    <p class="text-3xl">Hi, I'm Rajan. I am a web developer</p>
+    <p class="text-3xl">
+      Hi, I'm Rajan. I am a web developer from California but I tend to move
+      around alot. Currently I am in {currentLocation.city}, {currentLocation.country}
+    </p>
   </span>
 </div>
