1f922451646e693ff2023f4b883606c54243eb20
Rajan Singh <rajan1992@hotmail.com>
2021-03-17 13:42:49 -0400

    height again

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -7,8 +7,7 @@
   export let segment: string;
 </script>
 
-<div
-  class="bg-white dark:bg-black text-black dark:text-white h-min-screen h-full">
+<div class="bg-white dark:bg-black text-black dark:text-white min-h-screen">
   <div class="relative max-w-screen-xl mx-auto">
     <Nav {segment} />
     <main class="p-8">
